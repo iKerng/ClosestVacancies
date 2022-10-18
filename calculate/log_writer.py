515a3345d8b22639ebe15e31c4e0a22161d36949
iKerng <ikerng@gmail.com>
2022-10-18 15:23:19 +0400

    Исправление ошибки присвоения названия файла логирования

--- calculate/log_writer.py
+++ calculate/log_writer.py
@@ -1,9 +1,9 @@
 from os import path, getcwd
 import datetime as dt
 
-path_to_log_file = path.abspath(getcwd() + '/logs/' + dt.datetime.utcnow().strftime('%Y-%m-%d') + '.log')
-
 def to_log(log_user='Application', log_user_id=0, log_text=''):
+    path_to_log_file = path.abspath(getcwd() + '/logs/' + dt.datetime.utcnow().strftime('%Y-%m-%d') + '.log')
+    print(path_to_log_file)
     cur_time = dt.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')
     log_text = log_text.replace('\r\n','<new_line>').replace('\r','<new_line>').replace('\n','<new_line>')
     with open(path_to_log_file, 'a', encoding='utf8') as file:
